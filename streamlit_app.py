ae25c3507d296a85aed889cd5a8e96ace0350260
WhereIsMyPhone <112136358+monamiesanyal@users.noreply.github.com>
2022-08-26 01:32:21 +0100

    Update streamlit_app.py
    New header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
