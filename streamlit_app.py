add08f64ee062d1bec871e8c521b8a67e5dbebe6
WhereIsMyPhone <112136358+monamiesanyal@users.noreply.github.com>
2022-08-26 01:31:14 +0100

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
-streamlit.text('Hard-boiled Free-Range Egg')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text('🥑🍞 Avocado Toast')
